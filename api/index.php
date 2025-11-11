0596b55edd7e3bd1c148a67761cb269bd2f6ef81
Fordyce Gozali <forddyce92@gmail.com>
2025-11-11 22:36:51 +0700

    debug: test endpoint 2

--- api/index.php
+++ api/index.php
@@ -1,19 +1,21 @@
 <?php
 
-define('LARAVEL_START', microtime(true));
+require __DIR__.'/_init.php';
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+$app->useStoragePath('/tmp');
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
-)->send();
+);
+
+$response->send();
 
 $kernel->terminate($request, $response);
