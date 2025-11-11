cef79c00399b8bfc2a8f928b0f0b97cfd97fc466
Fordyce Gozali <forddyce92@gmail.com>
2025-11-11 22:18:04 +0700

    fix: proper vercel configuration

---
+++ storage/framework/views/ee693560acf6c2d766884a109208c46c.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dating App API</title>
+</head>
+<body>
+    <h1>Dating App API</h1>
+    <p>API is running successfully!</p>
+    <p><a href="/api/documentation">View API Documentation</a></p>
+</body>
+</html>
+<?php /**PATH /mnt/data/web/date/be/resources/views/welcome.blade.php ENDPATH**/ ?>
