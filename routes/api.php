77ed064700e17b9009da068c1b709047461599d2
Fordyce Gozali <forddyce92@gmail.com>
2025-11-11 22:33:52 +0700

    debug: test endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PersonController;
 use App\Http\Controllers\LikeController;
 
+Route::get('/test', function () {
+    return response()->json([
+        'status' => 'ok',
+        'message' => 'API is working',
+        'laravel' => app()->version()
+    ]);
+});
+
 Route::get('/people/recommended', [PersonController::class, 'recommended']);
 Route::post('/people/{person}/like', [LikeController::class, 'like']);
 Route::post('/people/{person}/dislike', [LikeController::class, 'dislike']);
